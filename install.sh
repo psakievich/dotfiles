2b0ce52babb80b8ed35889642630350fcffef0e3
psakiev <psakiev@sandia.gov>
2025-07-09 18:47:42 -0600

    Simplify logic

--- install.sh
+++ install.sh
@@ -1,17 +1,10 @@
 #!/bin/bash -l
 
-# export bash stuff check to see if we've already appended file
-# dotfileSource=$(grep .dotprofile ${HOME}/.bash_profile)
-# if [ -z "${dotfileSource}" ]; then
-#   echo "Appending .bash_profile"
-#   echo "source ${HOME}/.dotprofile" >> ${HOME}/.bash_profile
-# fi
-
-# directories
-mkdir -p ${HOME}/soft
 export DOTFILES="$( cd -- "$(dirname "${BASH_SOURCE[0]:-${(%):-%x}}")" >/dev/null 2>&1 ; pwd -P )"
 
-git submodule update --init --recursive
+git -C ${DOTFILES:?} submodule update --init --recursive
+
+pushd ${DOTFILES:?}
 
 source spack/share/spack/setup-env.sh
 export SPACK_DISABLE_LOCAL_CONFIG=1
@@ -20,21 +13,17 @@ pushd spack-manager
 ./install.sh --scope site
 popd
 
-# clone spack and activate it
-spack mirror add develop-bootstrap-aarch64-darwin https://binaries.spack.io/develop/bootstrap-aarch64-darwin
-spack bootstrap now --dev
-# spack gpg init
-# spack gpg create $user ${GPG_EMAIL:?}
-
 # create spack environments to install software
 pushd spack_environments
-envs=(*)
-for env in "${envs[@]}"
-do
+for env in */ ; do
+  # machine specific impl of env
   spack manager create-env --name $env --yaml $env/spack.yaml
+  # env specific buildcaches
   spack -e $env buildcache keys --install --trust
   spack -e $env install
+  # view for local bin's
   spack -e $env env view enable ${DOTFILES:?}/spack-views/{$env}-bin
+  # back up in buildcache
   spack -e $env buildcache push --unsigned ${DOTFILES:?}/spack-cache
 done
 popd
