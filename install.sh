3eab1044a8b3f6b1fb7201fe335ff86c3a7d6cd8
psakiev <psakiev@sandia.gov>
2025-07-22 12:26:33 -0600

    Update spack

--- install.sh
+++ install.sh
@@ -1,29 +1,34 @@
 #!/bin/bash -l
 
+cmd(){
+  echo "$@"
+  eval "$@"
+}
+
 export DOTFILES="$( cd -- "$(dirname "${BASH_SOURCE[0]:-${(%):-%x}}")" >/dev/null 2>&1 ; pwd -P )"
 
-git -C ${DOTFILES:?} submodule update --init --recursive
+cmd "git -C ${DOTFILES:?} submodule update --init --recursive"
 
-pushd ${DOTFILES:?}
+cmd "pushd ${DOTFILES:?}"
 
-source spack/share/spack/setup-env.sh
-export SPACK_DISABLE_LOCAL_CONFIG=1
+cmd "source spack/share/spack/setup-env.sh"
+cmd "export SPACK_DISABLE_LOCAL_CONFIG=1"
 
-pushd spack-manager
-./install.sh --scope site
-popd
+cmd "pushd spack-manager"
+cmd "./install.py --scope site"
+cmd "popd"
 
 # create spack environments to install software
-pushd spack_environments
+cmd "pushd spack_environments"
 for env in * ; do
   # machine specific impl of env
-  spack manager create-env --name ${env} --yaml ${env}/spack.yaml
+  cmd "spack manager create-env --name ${env} --yaml ${env}/spack.yaml"
   # env specific buildcaches
-  spack -e ${env} buildcache keys --install --trust
-  spack -e ${env} install
+  cmd "spack -e ${env} buildcache keys --install --trust"
+  cmd "spack -e ${env} install"
   # view for local bin's
-  spack -e ${env} env view enable ${DOTFILES:?}/spack-views/${env}-bin
+  cmd "spack -e ${env} env view enable ${DOTFILES:?}/spack-views/${env}-bin"
   # back up in buildcache
-  spack -e ${env} buildcache push --unsigned ${DOTFILES:?}/spack-cache
+  cmd "spack -e ${env} buildcache push --unsigned ${DOTFILES:?}/spack-cache"
 done
-popd
+cmd "popd"
