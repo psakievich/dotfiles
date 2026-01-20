a18b5a0962f1afd7bbd5186f43fcd4c2b6b8d204
psakievich <psakievich@gmail.com>
2026-01-19 21:42:20 -0500

    Reduce neovim plugins
    Prepare to redo the plugin architecture

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,8 +1,5 @@
 require("core")
 
--- [[ Install `lazy.nvim` plugin manager ]]
---    https://github.com/folke/lazy.nvim
---    `:help lazy.nvim.txt` for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system {
@@ -16,18 +13,9 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
--- [[ Configure plugins ]]
--- NOTE: Here is where you install your plugins.
---  You can configure plugins using the `config` key.
---
---  You can also configure plugins after the setup call,
---    as they will be available in your neovim runtime.
 require('lazy').setup('plugins')
 
 -- TODO move auto
 function CommentLinesContaining(str)
     vim.cmd('g/' .. str .. '/s/^/#/')
 end
-
--- The line beneath this is called `modeline`. See `:help modeline`
--- vim: ts=2 sts=2 sw=2 et
