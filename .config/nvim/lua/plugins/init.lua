04379de65b1b3aabd6b9eaeeafa433aadd15e398
psakievich <psakievich@gmail.com>
2026-01-19 21:42:20 -0500

    Reduce neovim plugins
    Prepare to redo the plugin architecture

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -1,23 +1,6 @@
 return {
-  { "nvim-tree/nvim-web-devicons", opts = {} },
-  { 'echasnovski/mini.icons', version = false },
   -- Git related plugins
-  -- 'tpope/vim-abolish (not currently using but plan to revisit)
-  'tpope/vim-fugitive',
-  'tpope/vim-commentary',
-  'tpope/vim-rhubarb',
   'christoomey/vim-tmux-navigator',
-  -- countdown timer
-  {
-  'cbrgm/countdown.nvim',
-    opts = {
-      default_minutes = 15,
-    },
-  },
-
-  -- Detect tabstop and shiftwidth automatically
-  'tpope/vim-sleuth',
-
 
   {
     -- Set lualine as statusline
@@ -31,24 +14,9 @@ return {
         section_separators = '',
       },
     },
-    setup = {
-      tabline = {
-        -- add the timer to the tabline bar
-        -- lualine_x = { timer, "encoding", "fileformat", "filetype" },
-      },
-    },
-  },
-
-  {
-    -- Add indentation guides even on blank lines
-    'lukas-reineke/indent-blankline.nvim',
-    -- Enable `lukas-reineke/indent-blankline.nvim`
-    -- See `:help ibl`
-    main = 'ibl',
-    opts = {},
-  },
 
   -- "gc" to comment visual regions/lines
   { 'numToStr/Comment.nvim', opts = {} },
 
+}
 }
