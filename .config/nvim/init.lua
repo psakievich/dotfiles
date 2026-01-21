346a06c73d74f6be05d4a8c03f71fd0ac250429c
psakievich <psakievich@gmail.com>
2026-01-20 22:49:51 -0500

    Base config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,21 +1,44 @@
-require("core")
+-- :help [anything]
+-- <Ctrl>] to dive on help links in neovim
 
-local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system {
-    'git',
-    'clone',
-    '--filter=blob:none',
-    'https://github.com/folke/lazy.nvim.git',
-    '--branch=stable', -- latest stable release
-    lazypath,
-  }
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+vim.o.swapfile = false
+vim.o.mouse = 'a'
+vim.o.undofile = true
+
+vim.o.relativenumber = true
+vim.o.number = true
+vim.o.wrap = false
+vim.o.cursorline = true
+
+-- Performance 
+vim.o.updatetime = 250
+vim.o.timeoutlen = 300
+
+-- Search
+vim.o.hlsearch = true
+vim.o.incsearch = true
+vim.o.ignorecase = true
+vim.o.smartcase = true
+vim.o.completeopt = "fuzzy,menuone,preview"
+
+vim.cmd.colorscheme("industry")
+
+-- hooks for switching modes from personal dev to pairing and back
+develop_mode = function()
+  vim.wo.relativenumber = true
+  vim.wo.number = true
+  vim.wo.cursorline = false
+  vim.o.hlsearch = false
 end
-vim.opt.rtp:prepend(lazypath)
-
-require('lazy').setup('plugins')
-
--- TODO move auto
-function CommentLinesContaining(str)
-    vim.cmd('g/' .. str .. '/s/^/#/')
+vim.api.nvim_create_user_command("DevMode", "lua develop_mode()", {})
+--
+pairing_mode = function()
+  vim.wo.relativenumber = false
+  vim.wo.number = true
+  vim.wo.cursorline = true
+  vim.o.hlsearch = true
 end
+vim.api.nvim_create_user_command("PairMode", "lua pairing_mode()", {})
