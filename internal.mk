137aeecca4a2a879e6525520ad911f3c43fbf868
psakievich <psakievich@gmail.com>
2026-01-26 21:34:54 -0500

    Update Makefile directive

---
+++ internal.mk
@@ -0,0 +1,50 @@
+
+.PHONY: all clean wipe
+
+SPACK_ROOT ?= spack
+
+SPACK := $(SPACK_ROOT)/bin/spack
+ENV_PREFIX := $(SPACK_ROOT)/var/spack/environments
+MANAGER_CONFIG := $(SPACK_ROOT)/etc/spack/config.yaml
+SPACK_ENV_ROOT ?= $(SPACK_ROOT)/var/spack/environments
+TEMPLATE_ROOT ?= spack_environments
+
+MANAGED_ENVS ?= $(shell ls $(TEMPLATE_ROOT))
+MANAGED_YAML := $(foreach ME, $(MANAGED_ENVS), $(SPACK_ENV_ROOT)/$(ME)/spack.yaml)
+ENVS ?= MANAGED_ENVS
+
+.PRECIOUS: $(SPACK_ENV_ROOT)/%/spack.yaml $(SPACK_ENV_ROOT)/%/spack.lock
+
+.PHONY: all clean
+
+all: $(ENVS)
+clean:
+	$(info does nothing right now)
+
+stow: core
+	$(SPACK) -e $(<) build-env stow -- stow --target $(HOME) .
+
+$(ENVS): %: $(SPACK_ENV_ROOT)/%/spack.lock
+	$(SPACK) -e $(*) install --reuse
+	$(SPACK) -e $(*) env view enable $(PWD)/spack-views/$(*)-bin
+
+$(SPACK_ENV_ROOT)/%/spack.lock: $(SPACK_ENV_ROOT)/%/spack.yaml
+	$(SPACK) -e $(*) concretize
+
+$(MANAGED_YAML): $(SPACK_ENV_ROOT)/%/spack.yaml: $(TEMPLATE_ROOT)/%/spack.yaml | $(MANAGER_CONFIG)
+	$(SPACK) env rm -y $(*)
+	$(SPACK) manager create-env --name $(*) --yaml $(<) 
+
+$(MANAGER_CONFIG): spack-manager/spack-manager.yaml
+	$(SPACK) config --scope spack add "config:extensions:[$(PWD)/spack-manager]"
+	$(SPACK) manager add dot-manager
+
+%-clean:
+	rm $($(SPACK) location -e $(*))/spack.lock
+
+%-wipe:
+	$(SPACK) env rm -y $(*)
+
+wipe:
+	rm -rf $(ENV_PREFIX)
+
