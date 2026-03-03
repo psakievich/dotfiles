0c2b539cf43564a6c794b5024c4c35831fee781d
psakievich <psakievich@gmail.com>
2026-03-02 23:58:23 -0500

    Update dependency logic/example

--- shell/entry.sh
+++
@@ -1,5 +0,0 @@
-# Things that should be sourced in a .bash_profile 
-export DOTFILES="$( cd -- "$(dirname "${BASH_SOURCE[0]:-${(%):-%x}}")" >/dev/null 2>&1 ; pwd -P )"
-source ${DOTFILES:-}/functions.sh
-source ${DOTFILES:-}/alias.sh
-source ${DOTFILES:-}/variables.sh
